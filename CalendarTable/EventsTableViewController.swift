c8f3d50d3c18000ad40ab13f3cb837c4f778f90f
hank1106 <hankzhang06@gmail.com>
2016-03-11 14:50:22 -0600

    bug fix

--- CalendarTable/EventsTableViewController.swift
+++ CalendarTable/EventsTableViewController.swift
@@ -87,17 +87,7 @@ class EventsTableViewController: UITableViewController {
         }
         return cell
     }
-    /*
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
-        //let storyboard = UIStoryboard(name: "storyboard", bundle: nil);
-        //let days = storyboard.instantiateViewControllerWithIdentifier("DaysTableViewController") as! UITableViewController
-        //let destinationVC:DaysTableViewController = DaysTableViewController()
-        
-        self.performSegueWithIdentifier("toAdd", sender: nil)
-    }
-    */
-
-
+    
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
